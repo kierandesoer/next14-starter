0bea941dd286d29a5a501d282b04d24d36d7bdfd
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2025-01-10 13:53:13 +0000

    Push to check NextAuth 500 status error

---
+++ src/app/api/auth/temp.ts
@@ -0,0 +1,4 @@
+MONGO = mongodb+srv://admin:nextJs@nextjstraining.32k4m.mongodb.net/NextJsTutorial?retryWrites=true&w=majority&appName=NextJsTraining
+
+AUTH_SECRET = qQzgnZF6NskgwHVvUVW839duEDC7RlUrqTdgHooMzB0=
+AUTH_URL = http://localhost:3000/api/auth
